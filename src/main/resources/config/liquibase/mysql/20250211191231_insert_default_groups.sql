f517622787cd80abd053897a72fd1d1d60bfbde3
djordje.markovic <djordjemarkovic997@gmail.com>
2025-02-11 22:43:14 +0100

    default data populating

---
+++ src/main/resources/config/liquibase/mysql/20250211191231_insert_default_groups.sql
@@ -0,0 +1,18 @@
+INSERT INTO jhi_group
+(id, name, enum_value_name, color, order_number, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(1, 'Seljene košnice', 'SELJENE', '#ffc654', 1, 1, 'group_1', '2025-01-12 20:56:41.074600', '2025-01-12 20:56:41.074600', '2025-01-12 21:46:59.649000', NULL, NULL);
+INSERT INTO jhi_group
+(id, name, enum_value_name, color, order_number, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(2, 'Košnice za zamenu matice', 'ZA_ZAMENU_MATICE', '#e1f74f', 2, 2, 'group_2', '2025-01-12 20:56:41.074600', '2025-01-12 20:56:41.074600', '2025-01-12 21:46:59.666000', NULL, NULL);
+INSERT INTO jhi_group
+(id, name, enum_value_name, color, order_number, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(3, 'Košnice dobre u paši', 'DOBRE_U_PASI', '#78fc3f', 3, 3, 'group_3', '2025-01-12 20:56:41.074700', '2025-01-12 20:56:41.074700', '2025-01-12 21:46:59.680000', NULL, NULL);
+INSERT INTO jhi_group
+(id, name, enum_value_name, color, order_number, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(4, 'Košnice za reprodukciju', 'ZA_REPRODUKCIJU', '#4bfac0', 4, 4, 'group_4', '2025-01-12 20:56:41.074700', '2025-01-12 20:56:41.074700', '2025-01-12 21:46:59.693000', NULL, NULL);
+INSERT INTO jhi_group
+(id, name, enum_value_name, color, order_number, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(5, 'Košnice za kontrolu', 'ZA_KONTROLU', '#51b3f5', 5, 5, 'group_5', '2025-01-12 20:56:41.074700', '2025-01-12 20:56:41.074700', '2025-01-12 21:46:59.710000', NULL, NULL);
+INSERT INTO jhi_group
+(id, name, enum_value_name, color, order_number, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(6, 'Košnice za dohranu', 'ZA_DOHRANU', '#fa5058', 6, 6, 'group_6', '2025-01-12 20:56:41.074700', '2025-01-12 20:56:41.074800', '2025-01-12 21:46:59.731000', NULL, NULL);
---
+++ src/main/resources/config/liquibase/mysql/20250211191634_insert_default_app_config.sql
@@ -0,0 +1,9 @@
+INSERT INTO app_config
+(id, jhi_key, type, value, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(1, 'AUTOMATIC_THEME', 'BOOLEAN', 'true', 1, 'group_1', '2025-01-12 20:56:41.074600', '2025-01-12 20:56:41.074600', '2025-01-12 21:46:59.649000', NULL, NULL);
+INSERT INTO app_config
+(id, jhi_key, type, value, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(2, 'MANUAL_LIGHT_THEME', 'BOOLEAN', 'true', 2, 'group_2', '2025-01-12 20:56:41.074600', '2025-01-12 20:56:41.074600', '2025-01-12 21:46:59.666000', NULL, NULL);
+INSERT INTO app_config
+(id, jhi_key, type, value, external_id, uuid, date_created, date_modified, date_synched, date_deleted, user_id)
+VALUES(3, 'HAS_SERVER_CONNECTION', 'BOOLEAN', 'true', 3, 'group_3', '2025-01-12 20:56:41.074700', '2025-01-12 20:56:41.074700', '2025-01-12 21:46:59.680000', NULL, NULL);
